11a8531d930d1e9e9783def36bbb83bb91809780
varisshaaaa <varishasid06@gmail.com>
2025-12-14 20:24:39 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  root: "public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "public/index.html")
+    }
   }
 })
